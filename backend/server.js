c44103fa90536c7657253af6a4fcc7546567f80a
phunguyen233 <phunguyen23306@gmail.com>
2025-12-09 02:53:10 +0700

    sửa server.js

--- backend/server.js
+++ backend/server.js
@@ -33,4 +33,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server chạy tại http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server chạy tại ${PORT}`));
